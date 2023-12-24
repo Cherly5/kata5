bd4a39c9f56db3c608ae89e04cdfc009b0d83b68
Echeyde Ramos Caballero <echeyde.ramos@gmail.com>
2023-12-24 18:08:37 +0000

    Commit Inicial

---
+++ src/main/java/org/example/Command.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public interface Command {
+
+    Output execute(Input input);
+    interface Input {
+        String get(String key);
+    }
+
+    interface Output {
+        int response();
+        String result();
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import spark.Spark;
+
+public class Main {
+    public static void main(String[] args) {
+        SparkCommandExecutor.put("factorial", new FactorialCommand());
+        Spark.port(8080);
+        Spark.get("/factorial",(req,res) -> SparkCommandExecutor
+                .with(req,res)
+                .execute("factorial"));
+    }
+
+}
